d358be9266f53ec55e148ad05538bff68e63071d
holywaterBetter <holywater.better@gmail.com>
2024-01-14 23:41:44 +0900

    feat(#3): managerService 테스트 모드 해제

--- src/app/manager.service.ts
+++ src/app/manager.service.ts
@@ -51,6 +51,8 @@ export class ManagerService implements IManagerService {
                 if (snapshot.size === 1) {
                     this._permission = true;
                     this._subscribeUserDB();
+                    //TEST MODE
+                    // this._testMode();
                 }
                 return snapshot.size === 1;
             })
@@ -65,104 +67,94 @@ export class ManagerService implements IManagerService {
         if (this.customerDB$.getValue().length > 0) return;
 
         //확정된 예약에 대해 sub을 걸고, 개별 변경 대응
-        // this.store
-        //     .collection(BOOKING_COLLECTION)
-        //     .ref.where('status', '!=', 'ready')
-        //     .get()
-        //     .then((snapshot) => {
-        //         snapshot.forEach((doc: QueryDocumentSnapshot<any>) => {
-        //             doc.ref.onSnapshot((v) => {
-        //                 let db = this.customerDB$.getValue();
-        //                 let user = v.data() as CustomerInfo;
-        //                 user.date = Moment(user.date.toDate());
-        //                 user.person = Number(user.person) | 0;
-        //                 user.kids = Number(user.kids) | 0;
-        //                 user.flatTable = Number(user.flatTable) | 0;
-        //                 user.dechTable = Number(user.dechTable) | 0;
+        this.store
+            .collection(BOOKING_COLLECTION)
+            .ref.where('status', '!=', 'ready')
+            .get()
+            .then((snapshot) => {
+                snapshot.forEach((doc: QueryDocumentSnapshot<any>) => {
+                    doc.ref.onSnapshot((v) => {
+                        let db = this.customerDB$.getValue();
+                        let user = v.data() as CustomerInfo;
+                        user.date = Moment(user.date.toDate());
+                        user.person = Number(user.person) | 0;
+                        user.kids = Number(user.kids) | 0;
+                        user.flatTable = Number(user.flatTable) | 0;
+                        user.dechTable = Number(user.dechTable) | 0;
 
-        //                 if (
-        //                     db.filter((oldUser) => oldUser['id'] === user.id)[0]
-        //                 ) {
-        //                     const index = db.findIndex(
-        //                         (oldUser) => oldUser.id === user.id
-        //                     );
-        //                     db[index] = user;
-        //                     db = db.filter((item) => item);
-        //                     this.customerDB$.next(
-        //                         db.sort((a, b) => this._sortList(a, b))
-        //                     );
-        //                 } else {
-        //                     this.customerDB$.next(
-        //                         [...db, user].sort((a, b) =>
-        //                             this._sortList(a, b)
-        //                         )
-        //                     );
-        //                 }
-        //             });
-        //         });
-        //     });
+                        if (
+                            db.filter((oldUser) => oldUser['id'] === user.id)[0]
+                        ) {
+                            const index = db.findIndex(
+                                (oldUser) => oldUser.id === user.id
+                            );
+                            db[index] = user;
+                            db = db.filter((item) => item);
+                            this.customerDB$.next(
+                                db.sort((a, b) => this._sortList(a, b))
+                            );
+                        } else {
+                            this.customerDB$.next(
+                                [...db, user].sort((a, b) =>
+                                    this._sortList(a, b)
+                                )
+                            );
+                        }
+                    });
+                });
+            });
 
         // // 신규 DB(ready)는 새로 sub을 걸어야 해.
-        // this.store
-        //     .collection(BOOKING_COLLECTION, (ref) =>
-        //         ref.where('status', '==', 'ready')
-        //     )
-        //     .snapshotChanges()
-        //     .subscribe((actions) => {
-        //         actions.forEach((action) => {
-        //             const id = action.payload.doc.id;
-        //             //신규인 경우만 새로 Sub
-        //             if (
-        //                 this.customerDB$
-        //                     .getValue()
-        //                     .filter((u) => u['id'] === id).length === 0
-        //             ) {
-        //                 this.store
-        //                     .collection(BOOKING_COLLECTION)
-        //                     .doc(id)
-        //                     .ref.onSnapshot((v) => {
-        //                         let db = this.customerDB$.getValue();
-        //                         let user = v.data() as CustomerInfo;
-        //                         user.date = Moment(user.date.toDate());
-        //                         user.person = Number(user.person) | 0;
-        //                         user.kids = Number(user.kids) | 0;
-        //                         user.flatTable = Number(user.flatTable) | 0;
-        //                         user.dechTable = Number(user.dechTable) | 0;
+        this.store
+            .collection(BOOKING_COLLECTION, (ref) =>
+                ref.where('status', '==', 'ready')
+            )
+            .snapshotChanges()
+            .subscribe((actions) => {
+                actions.forEach((action) => {
+                    const id = action.payload.doc.id;
+                    //신규인 경우만 새로 Sub
+                    if (
+                        this.customerDB$
+                            .getValue()
+                            .filter((u) => u['id'] === id).length === 0
+                    ) {
+                        this.store
+                            .collection(BOOKING_COLLECTION)
+                            .doc(id)
+                            .ref.onSnapshot((v) => {
+                                let db = this.customerDB$.getValue();
+                                let user = v.data() as CustomerInfo;
+                                user.date = Moment(user.date.toDate());
+                                user.person = Number(user.person) | 0;
+                                user.kids = Number(user.kids) | 0;
+                                user.flatTable = Number(user.flatTable) | 0;
+                                user.dechTable = Number(user.dechTable) | 0;
 
-        //                         if (
-        //                             db.filter(
-        //                                 (oldUser) => oldUser['id'] === user.id
-        //                             )[0]
-        //                         ) {
-        //                             const index = db.findIndex(
-        //                                 (oldUser) => oldUser.id === user.id
-        //                             );
-        //                             db[index] = user;
-        //                             db = db.filter((item) => item);
-        //                             this.customerDB$.next(
-        //                                 db.sort((a, b) => this._sortList(a, b))
-        //                             );
-        //                         } else {
-        //                             this.customerDB$.next(
-        //                                 [...db, user].sort((a, b) =>
-        //                                     this._sortList(a, b)
-        //                                 )
-        //                             );
-        //                         }
-        //                     });
-        //             }
-        //         });
-        //     });
-
-        //TEST MODE
-        const customerDB: CustomerInfo[] = [];
-        DB.forEach((db) => {
-            customerDB.push({
-                ...(db as any),
-                date: Moment(db.date),
+                                if (
+                                    db.filter(
+                                        (oldUser) => oldUser['id'] === user.id
+                                    )[0]
+                                ) {
+                                    const index = db.findIndex(
+                                        (oldUser) => oldUser.id === user.id
+                                    );
+                                    db[index] = user;
+                                    db = db.filter((item) => item);
+                                    this.customerDB$.next(
+                                        db.sort((a, b) => this._sortList(a, b))
+                                    );
+                                } else {
+                                    this.customerDB$.next(
+                                        [...db, user].sort((a, b) =>
+                                            this._sortList(a, b)
+                                        )
+                                    );
+                                }
+                            });
+                    }
+                });
             });
-        });
-        this.customerDB$.next(customerDB);
     }
 
     private _sortList(a: CustomerInfo, b: CustomerInfo) {
@@ -243,5 +235,16 @@ export class ManagerService implements IManagerService {
                     '삭제 과정에서 서버 오류 발생 :' + id + ', ' + e
                 );
             });
+    }
+
+    private _testMode() {
+        const customerDB: CustomerInfo[] = [];
+        DB.forEach((db) => {
+            customerDB.push({
+                ...(db as any),
+                date: Moment(db.date),
+            });
+        });
+        this.customerDB$.next(customerDB);
     }
 }
