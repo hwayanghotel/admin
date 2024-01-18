22b4c302997512cd764578fc305441626037f4de
holywaterBetter <holywater.better@gmail.com>
2024-01-19 06:55:18 +0900

    feat(#3): bookingTimeList 추가

---
+++ src/app/booking-time-list/booking-time-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'booking-time-list',
+    templateUrl: './booking-time-list.component.html',
+    styleUrls: ['./booking-time-list.component.scss'],
+})
+export class BookingTimeListComponent {}
